cf2c4f6b22fa699f459b3e307615f174b6c61ee9
Leafuke <zhuolin.yew@outlook.com>
2025-11-30 10:06:18 +0800

    fix

--- scripts/post-build/external-links/task-handler.ts
+++ scripts/post-build/external-links/task-handler.ts
@@ -2,7 +2,7 @@ import { HTMLElement } from "node-html-parser";
 
 import { TaskHandler } from "../html-postprocess.js";
 
-const ROOT_URL = new URL("https://oi-wiki.org/");
+const ROOT_URL = new URL("https://physics-learning-wiki.github.io/Physics-Learning-Wiki/");
 
 export const taskHandler = new (class implements TaskHandler<void> {
   async process(document: HTMLElement) {
